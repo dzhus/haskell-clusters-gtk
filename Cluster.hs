bc37c0bfb5d95842920019f8f81ed48f61836284
Dmitry Dzhus <dima@dzhus.org>
2011-10-23 19:19:38 +0400

    Simple proximity-based clusterization

---
+++ Cluster.hs
@@ -0,0 +1,48 @@
+module Cluster
+    where
+
+type Point = (Double, Double)
+data Cluster = Cluster
+               {
+                 center :: Point,
+                 elements :: [Point],
+                 threshold :: Double
+               }
+
+instance Show Cluster where
+         show c = show (elements c) ++ " @ " ++ show (center c)
+
+-- replaceIdx l i e changes i-th element in l to e. i is 1-based.
+-- replace is a lie.
+replaceIdx (x:xs) 0 e = e:xs
+replaceIdx [] i e = []
+replaceIdx (x:xs) i e = x:(replaceIdx xs (i - 1) e)
+
+distance p1 p2 = sqrt ((fst p1 - fst p2) ^ 2 + (snd p1 - snd p2) ^ 2)
+
+addToCluster cluster point = cluster{elements = point:(elements cluster)}
+newCluster point threshold = Cluster point [] threshold
+        
+classify :: Point -> [Cluster] -> Double -> [Cluster]
+classify point clusters threshold = 
+    let
+        distances = zip [distance point (center c) | c <- clusters] [0..]
+        -- minDist is a pair of minimal distance to cluster and its index;
+        -- if no cluster is within threshold radius is found, index is -1
+        minDist   = foldl (\p n -> if (fst p) <= (fst n) then p else n) (threshold, -1) distances
+        clusterId = snd minDist
+    in
+      if clusterId == -1 then
+      -- Add new cluster around this point
+          (newCluster point threshold):clusters
+      else
+      -- Add point to existing cluster
+        replaceIdx clusters clusterId (addToCluster (clusters !! clusterId) point)
+
+clusterize :: [Point] -> Double -> [Cluster]
+clusterize points threshold =
+    let
+        clusterize1 (p:points) clusters = clusterize1 points (classify p clusters threshold)
+        clusterize1 [] clusters = clusters
+    in
+      clusterize1 points []
---
+++ main.hs
@@ -0,0 +1,72 @@
+module Main where
+
+import Control.Monad
+import Data.Accessor
+import Data.Maybe
+import IO
+
+import Graphics.UI.Gtk
+import Graphics.Rendering.Cairo
+
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Gtk
+
+import Data.Colour
+import Data.Colour.Names
+
+import Cluster
+
+getLines = liftM lines . readFile
+
+readPoint s = read s::Cluster.Point
+
+getPointsFromFile :: Maybe FilePath -> IO (Maybe [Cluster.Point])
+getPointsFromFile Nothing = do return Nothing
+getPointsFromFile (Just filename) =
+  do lines <- getLines filename
+     return (Just (map readPoint lines))
+
+getPointsFromBuffer :: TextBuffer -> IO (Maybe [Cluster.Point])
+getPointsFromBuffer buf =
+  do bounds <- textBufferGetBounds buf
+     text <- textBufferGetText buf (fst bounds) (snd bounds) True
+     if text /= "" then
+          return (Just (map readPoint (filter (/= "") (lines text))))
+          else return Nothing
+
+main :: IO ()
+main = 
+  do initGUI
+     builder <- builderNew
+     builderAddFromFile builder "window.ui"
+
+     window <- builderGetObject builder castToWindow "window"
+     canvas <- builderGetObject builder castToDrawingArea "canvas"
+     button <- builderGetObject builder castToButton "go"
+     textview <- builderGetObject builder castToTextView "textview"
+     
+     widgetShowAll window
+
+     drawin <- widgetGetDrawWindow canvas
+     widgetModifyBg canvas StateNormal (Color 65535 65535 65535)
+
+     onDestroy window mainQuit
+     onClicked button (do buf <- textViewGetBuffer textview
+                          points <- getPointsFromBuffer buf
+                          let clusters = (clusterize (fromJust points) 5.0) in
+                              updateCanvas (toRenderable (plotClusters clusters)) canvas
+                          return ())
+     mainGUI
+
+plotClusters :: [Cluster.Cluster] -> Layout1 Double Double
+plotClusters clusters =
+  layout
+  where
+    point_plots = [plot_points_style ^= filledCircles 5 (opaque red) $
+                   plot_points_values ^= (elements c) $
+                   defaultPlotPoints | c <- clusters]
+    clusters_plot = area_spots_fillcolour ^= (withOpacity blue 0.5) $
+                    area_spots_max_radius ^= 5^2 $
+                    area_spots_values ^= [(fst (center c), snd (center c), threshold c) | c <- clusters] $ 
+                    defaultAreaSpots
+    layout = layout1_plots ^= (map (Left . toPlot) point_plots)++[Left (toPlot clusters_plot)] $ defaultLayout1
